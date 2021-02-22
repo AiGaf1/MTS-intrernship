59220293e8acf34f523814b4fed3ef30769617ba
AiGaf1 <33295142+AiGaf1@users.noreply.github.com>
2021-02-22 12:54:26 +0300

    Add files via upload

--- demo_camera_V2.0.py
+++ demo_camera_V2.0.py
@@ -1,30 +1,72 @@
 import argparse
-import logging
 import time
-
 import cv2
 import numpy as np
-
 from tf_pose.estimator import TfPoseEstimator
 from tf_pose.networks import get_graph_path, model_wh
 import scripts.label_image as label_img
+import copy
+from tensorflow.keras.models import load_model
+from PyQt5 import *
+import PyQt5
 
-logger = logging.getLogger('TfPoseEstimator-WebCam')
-logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
-ch.setFormatter(formatter)
-logger.addHandler(ch)
+#import tensorflow
+
 
 fps_time = 0
+minusX = 110
+minusY = 150
+H = 224
+W = 224
+
+# General Settings
+prediction = ''
+action = ''
+score = 0
+img_counter = 500
+
+
+gesture_names = {0: 'Fist',
+                 1: 'L',
+                 2: 'Okay',
+                 3: 'Palm',
+                 4: 'Peace'}
+
+model = load_model('/home/aigaf/Desktop/MTS-Internship/models/VGG_cross_validated.h5')
+
+
+def predict_rgb_image(img):
+    result = gesture_names[model.predict_classes(img)[0]]
+    print(result)
+    return (result)
+
+
+def predict_rgb_image_vgg(image):
+    image = np.array(image, dtype='float32')
+    image /= 255
+    pred_array = model.predict(image)
+    #print(f'pred_array: {pred_array}')
+    result = gesture_names[np.argmax(pred_array)]
+    #print(f'Result: {result}')
+    #print(max(pred_array[0]))
+    score = float("%0.2f" % (max(pred_array[0]) * 100))
+    print(result)
+    return result, score
+
+
+#cap_region_x_begin = 0.5  # start point/total width
+#cap_region_y_end = 0.8  # start point/total width
+threshold = 60  # binary threshold
+blurValue = 41  # GaussianBlur parameter
+#bgSubThreshold = 50
+#learningRate = 0
+#isBgCaptured = 1  # bool, whether the background captured
+#triggerSwitch = False  # if true, keyboard simulator works
+#prediction_arr = []
+
+
 def cropHands(points, frame):
     # define croping values
-    minusX = 70
-    minusY = 70
-    h = 200
-    w = 200
-
     # output
     if (points[0] >= minusX and points[1] >= minusY):
         x = points[0] - minusX
@@ -34,7 +76,7 @@ def cropHands(points, frame):
         y = points[1]
     
         # Croping the frame
-    crop_frame = frame[y:y+h, x:x+w]
+    crop_frame = frame[y:y+H, x:x+W]
 
     return crop_frame
 
@@ -53,45 +95,90 @@ if __name__ == '__main__':
                         help='for debug purpose, if enabled, speed for inference is dropped.')
     args = parser.parse_args()
 
-    w, h = model_wh(args.resize)
+    w, h = model_wh('0x0')
     if w > 0 and h > 0:
-        e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
+        e = TfPoseEstimator('/home/aigaf/pose-estimation-detection/models/graph/mobilenet_thin/graph_opt.pb', target_size=(w, h))
     else:
-        e = TfPoseEstimator(get_graph_path(args.model), target_size=(432, 368))
+        e = TfPoseEstimator('/home/aigaf/pose-estimation-detection/models/graph/mobilenet_thin/graph_opt.pb', target_size=(432, 368))
 
-    cam = cv2.VideoCapture(args.camera)
+    cam = cv2.VideoCapture(0)
 
-    count = 0
+    currentframe = 0
     while True:
     
         ret_val, image1 = cam.read()
         
-        humans = e.inference(image1, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
+        humans = e.inference(image1, resize_to_default=(w > 0 and h > 0), upsample_size=4.0)
 
         img = TfPoseEstimator.draw_humans(image1, humans, imgcopy=True)
         
         for human in humans:
             for i in human.body_parts.keys():
+                now = time.time()
+                future = now + 10
+                
                 if i == 7:
+                    
                     left_wrist_point = human.body_parts[7]
-                    print('wrist aaa')
-                    print(left_wrist_point.x)
-                    print(left_wrist_point.y)
                     image_h, image_w = image1.shape[:2]
                     center = (int(left_wrist_point.x * image_w + 0.5), int(left_wrist_point.y * image_h + 0.5))
-                    print(center)
+                    
                     crop_frame = cropHands(center, image1)
+
                     cv2.imshow('frame',crop_frame)
+                    
+                    
+                    #name = './images/frame' + str(currentframe) + '.jpg'
+                    #print ('Creating...' + name) 
+                    
+                    # writing the extracted images 
+                    
+                    gray = cv2.cvtColor(crop_frame, cv2.COLOR_RGB2GRAY)
+                    blur = cv2.GaussianBlur(gray, (blurValue, blurValue), 0)
+                    ret, thresh = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                    #cv2.putText(thresh, f"Prediction: {prediction} ({score}%)", (50, 30), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                    #    (255, 255, 255))
+                    #cv2.putText(thresh, f"Action: {action}", (50, 80), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                    #    (255, 255, 255))
+                    target = np.stack((thresh,) * 3, axis=-1)
+                    target = cv2.resize(target, (224, 224))
+                    target = target.reshape(1, 224, 224, 3)
+
+                    prediction, score = predict_rgb_image_vgg(target)
+                    
+                    if score > 0.99 and prediction == 'Fist':
+                        print("Разгрузка")
+
+                    #prediction_arr.append(prediction)
+
+                elif now > future:
+                    print("ALERT")
+                    
+                    #print(prediction)
+                    #cv2.imwrite(name, gray) 
+                    #currentframe += 1
+                    # increasing counter so that it will 
+                    # show how many frames are created 
+                    
+                    #result, score = predict(img)
+                    #cv2.destroyAllWindows()
+        
                 
         cv2.imshow('tf-pose-estimation result', img)
+        
         
         fps_time = time.time()
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-        count += 1
-
     cv2.destroyAllWindows()
+
+
+
+     
+
+
+
 
 # =============================================================================
 # For running the script simply run the following in the cmd prompt/terminal :
